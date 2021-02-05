624fe2a43fa20905eaab1ca6e8ff95c6d5887702
flpalacios93@gmail.comgit config --global user.name flap93git config --global user.name flpalacios93@gmail.com <flpalacios93@gmail.com>
2021-02-04 21:40:54 -0500

    in progress

--- lab-express-file-upload/models/picture.model.js
+++ lab-express-file-upload/models/picture.model.js
@@ -13,3 +13,7 @@ const pictureSchema = new Schema(
 const Picture = mongoose.model('Picture', pictureSchema);
  
 module.exports = Picture;
+
+
+
+
--- lab-express-file-upload/routes/index.js
+++ lab-express-file-upload/routes/index.js
@@ -2,10 +2,33 @@ const express = require('express');
 const router  = express.Router();
 const multer = require('multer');
 const Picture = require('../models/picture.model');
+const upload = multer({ dest: './public/uploads/' });
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+// Route to upload from project base path
+
+ 
+router.post('/upload', upload.single('photo'), (req, res, next) => {
+  const picture = new Picture({
+    name: req.body.name,
+    path: `/uploads/${req.file.filename}`,
+    originalName: req.file.originalname
+  });
+ 
+  picture
+    .save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => {
+      next(error);
+    });
+  
+
 
 module.exports = router;
